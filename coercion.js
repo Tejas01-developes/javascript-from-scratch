20e7349df1770a46a2342e8b592301c6837c744c
Tejas01-developes <tlimbad7@gmail.com>
2026-08-04 23:19:22 +0530

    coercion topics

---
+++ coercion.js
@@ -0,0 +1,22 @@
+// let str="42"
+// let num=Number(str)
+// console.log(typeof num)
+
+
+let result=1 + "is the answer"
+
+console.log(typeof result)
+
+// == loose equality for this "5" and 5 are same and 
+// === strict equality  it compares and strictly compare datatype also
+
+
+
+let res="5" -  1
+
+console.log(res)    // in this it tells that string does two things to join the two things like "5" + 1 becomes 51 but "5" - 1 becomes 4 so this part of the string is seen here
+
+
+let res2=[]+ []
+
+console.log(res2)
