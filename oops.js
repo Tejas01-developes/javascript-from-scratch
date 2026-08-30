04ed01165eacc855eb23e0e442a87928546ae6f3
Tejas01-developes <tlimbad7@gmail.com>
2026-08-30 23:16:57 +0530

    oops learning and exploring

---
+++ oops.js
@@ -0,0 +1,65 @@
+// class person{
+
+//     profession="IT"
+//     constructor(name,age){
+//         this.name=name
+//         this.age=age
+//     }
+// }
+
+// const p1=new person("tejas",21)
+// console.log(typeof p1)
+
+
+// class p1{
+//     behiviour(){
+//         console.log("Gentalmen")
+//     }
+// }
+
+// class p2 extends p1{
+//     behiviour(){
+//         super.behiviour()
+//         console.log("chapri")
+//     }
+// }
+
+// const p=new p2()
+
+// console.log(p.behiviour())
+
+
+// class users{
+//     static count=0
+
+//     constructor(name){
+//         this.name=name
+//         users.count++
+//     }
+//     static timepass(){
+//         return new users("tejas")
+//     }
+// }
+
+// const p1=new users("tejas")
+// const p2=new users("jainam")
+// const p3=users.timepass()
+// console.log(users.count)
+
+class p1{
+    constructor(degree){
+        this.degree=degree
+    }
+}
+
+class p2 extends p1{
+    constructor(degree,name,age){
+        super(degree)
+        this.name=name
+        this.age=age
+    }
+}
+
+const p=new p2("bscit","tejas",21)
+
+console.log(p.degree)
