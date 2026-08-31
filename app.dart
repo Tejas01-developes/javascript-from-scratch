908699799c7e067cbf8fbbd90aa691cb96ace2fb
Tejas01-developes <tlimbad7@gmail.com>
2026-08-31 22:26:51 +0530

    some dart basic syntex for the flutter

---
+++ app.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name="Tejas";
+  int age=21;
+  bool male=true;
+  List<dynamic> fruits=["apple",21];
+  Map<String,dynamic>mydata={
+    "name":name,
+    "age":age,
+    "fruits":fruits,
+  };
+  fruits.addAll(["jainam",20]);
+  print(mydata);
+}
