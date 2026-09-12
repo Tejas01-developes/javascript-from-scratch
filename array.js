31d06bc94184ea21818ac813e9f22fb8954c08a3
Tejas01-developes <tlimbad7@gmail.com>
2026-09-12 23:36:47 +0530

    dsa cooncept

--- array.js
+++ array.js
@@ -1,7 +1,23 @@
-const arr = [10, 20, 30, 40, 50]
-const res=[]
-for(let i=0;i < arr.length;i+=2){
-    res.push(arr[i])
-}
+// 1. Print Alternates
 
-console.log(res.toString())
+// const arr = [10, 20, 30, 40, 50]
+// const res=[]
+// for(let i=0;i < arr.length;i+=2){
+//     res.push(arr[i])
+// }
+// console.log(res.toString())
+
+
+// 2.Leaders in an array
+const arr= [16, 17, 4, 3, 5, 2]
+function findleader(array){
+let n=arr.length
+let leaders=[]
+
+let maxright=arr[n-1]
+leaders.push(maxright)
+
+for(let i=n-2;i>=0 ; i--){
+    
+}
+}
