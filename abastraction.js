d6df2609ed2692028704a6dd36406dff22a1edaa
Tejas01-developes <tlimbad7@gmail.com>
2026-09-19 20:56:41 +0530

    update

--- abastraction.js
+++ abastraction.js
@@ -14,3 +14,29 @@
 
 // const car=new startengin()
 // console.log(car.start("Defender"))
+
+// const p1={
+// name:"Tejas",
+// age:21,
+// profession:"IT"
+// }
+
+// p1["collage"]="kjit"
+
+
+
+
+
+
+// console.log(JSON.parse(p1))
+
+
+const a=[1,2,3,4,5,6,7,8,89,90]
+
+
+for(arr of a){
+    console.log(JSON.parse(arr))
+}
+
+
+
